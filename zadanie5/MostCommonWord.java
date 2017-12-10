8ef1728101a445589f365874a0c47664d00b8c0b
wrryy <Woj.rydzewski@gmail.com>
2017-12-10 12:35:21 +0100

    almost there

--- zadanie5/MostCommonWord.java
+++ zadanie5/MostCommonWord.java
@@ -1,7 +1,16 @@
 package zadanie5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -9,23 +18,85 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class MostCommonWord {
+import sun.misc.IOUtils;
+
+public class MostCommonWord implements Comparable<Integer> {
 
 	public static void main(String[] args) {
-		mostCommonWord();
+		// mostCommonWords();
+		mostCommonWords("http://www.onet.pl/");
 	}
 
-	static void mostCommonWord() {
-		Connection connect = Jsoup.connect("http://www.onet.pl/");
+	static void mostCommonWords(String url) {
+		mostcommonWords(url);
+	}
+
+	static void mostCommonWords() {
+		mostcommonWords("http://www.onet.pl/");
+	}
+
+	static void mostcommonWords(String url) {
+		Connection connect = Jsoup.connect(url);
 		try {
-			FileWriter fw = new FileWriter("popular_words.txt");
+			FileWriter fw = new FileWriter("popular_words.txt", false);
 			Document document = connect.get();
 			Elements links = document.select("span.title");
 			for (Element elem : links) {
-				fw.append(elem.toString()).append("\n");
+				fw.append(elem.text().toString()).append("\n");
 			}
+			HashMap<String, Integer> words = countWords("popular_words.txt");
+			firstTen(words);
+			// for(Map.Entry<String, Integer> entry: wordCounter.entrySet()){
+			// System.out.println(entry.getKey()+ " " + entry.getValue());
+			// }
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	static HashMap<String, Integer> countWords(String fileName) throws FileNotFoundException {
+		Scanner scan = new Scanner(new File(fileName));
+		HashMap<String, Integer> wordCounter = new HashMap<>();
+
+		while (scan.hasNextLine()) {
+			String[] line = scan.nextLine().split("[-.,\\s:\"!\\/?]");
+			for (String string : line) {
+				String s = string.toLowerCase();
+				if ((s != null) && (s.length() > 3)) {
+					if (!wordCounter.containsKey(s)) {
+						wordCounter.put(s, 1);
+					} else if (wordCounter.containsKey(s)) {
+						wordCounter.replace(s, wordCounter.get(s) + 1);
+					}
+				}
+			}
+		}
+		scan.close();
+		return wordCounter;
+	}
+
+	static void firstTen(HashMap<String, Integer> map) throws IOException {
+		FileWriter fw = new FileWriter("most_popular_words.txt");
+		/*
+		 * Stream<HashMap.Entry<String,Integer>> sorted =
+		 * map.entrySet().stream() .sorted(HashMap.Entry.comparingByValue())
+		 */ /* ascending */
+		int counter = 0;
+		while (counter < 10) {
+			Stream<HashMap.Entry<String, Integer>> sorted = map.entrySet().stream()
+					.sorted(Collections.reverseOrder(HashMap.Entry.comparingByValue())); /* descending */
+			// sorted.filter(s -> s.length() ==
+			// 10).forEach(System.out::println);
+			for (String s: sorted) {
+				fw.append(s);
+			}
+		}
+	}
+
+	@Override
+	public int compareTo(Integer o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
