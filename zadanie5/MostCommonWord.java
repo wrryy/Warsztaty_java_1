26def7fe7b16a18afa7c4ce6b1af015f945fbc6a
wrryy <Woj.rydzewski@gmail.com>
2017-12-10 22:15:26 +0100

    Ver2

--- zadanie5/MostCommonWord.java
+++ zadanie5/MostCommonWord.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.stream.Stream;
 
 import org.jsoup.Connection;
@@ -17,8 +17,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import sun.misc.IOUtils;
 
 public class MostCommonWord implements Comparable<Integer> {
 
@@ -44,20 +42,17 @@ public class MostCommonWord implements Comparable<Integer> {
 			for (Element elem : links) {
 				fw.append(elem.text().toString()).append("\n");
 			}
-			HashMap<String, Integer> words = countWords("popular_words.txt");
+			TreeMap<String, Integer> words = countWords("popular_words.txt");
 			firstTen(words);
-			// for(Map.Entry<String, Integer> entry: wordCounter.entrySet()){
-			// System.out.println(entry.getKey()+ " " + entry.getValue());
-			// }
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	static HashMap<String, Integer> countWords(String fileName) throws FileNotFoundException {
+	static TreeMap<String, Integer> countWords(String fileName) throws FileNotFoundException {
 		Scanner scan = new Scanner(new File(fileName));
-		HashMap<String, Integer> wordCounter = new HashMap<>();
+		TreeMap<String, Integer> wordCounter = new TreeMap<>();
 
 		while (scan.hasNextLine()) {
 			String[] line = scan.nextLine().split("[-.,\\s:\"!\\/?]");
@@ -76,23 +71,29 @@ public class MostCommonWord implements Comparable<Integer> {
 		return wordCounter;
 	}
 
-	static void firstTen(HashMap<String, Integer> map) throws IOException {
+	static void firstTen(TreeMap<String, Integer> map) throws IOException {
 		FileWriter fw = new FileWriter("most_popular_words.txt");
-		/*
-		 * Stream<HashMap.Entry<String,Integer>> sorted =
-		 * map.entrySet().stream() .sorted(HashMap.Entry.comparingByValue())
-		 */ /* ascending */
+		// Stream<HashMap.Entry<String, Integer>> sorted =
+		// map.entrySet().stream()
+		// .sorted(Collections.reverseOrder(HashMap.Entry.comparingByValue()));
 		int counter = 0;
-		while (counter < 10) {
-			Stream<HashMap.Entry<String, Integer>> sorted = map.entrySet().stream()
-					.sorted(Collections.reverseOrder(HashMap.Entry.comparingByValue())); /* descending */
-			// sorted.filter(s -> s.length() ==
-			// 10).forEach(System.out::println);
-			for (String s: sorted) {
-				fw.append(s);
+		for (Map.Entry<String, Integer> mapData : map.entrySet()) {
+			if (counter < 10) {
+				System.out.println("Key : " + mapData.getKey() + " Value : " + mapData.getValue());
+				counter++;
 			}
 		}
+		// while (counter < 1) {
+		// for (int i = 0; i<9; i++){
+		// System.out.println(l[i]);
+		// fw.append((sorted.toArray())[i].toString());
+		// }
+		// counter++;
+		// }
 	}
+	// fw.append((l[i]).toString());
+	// fw.append(l.get(i).toString());
+	// fw.append(().get(i).toString());
 
 	@Override
 	public int compareTo(Integer o) {
